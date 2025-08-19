885053e69d0f3ab28d132c4c33f3859c06437c05
spcameron <cameron.seanpatrick@gmail.com>
2025-08-19 12:35:47 -0400

    Define operate() function body

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ function div(x, y) {
   return x / y;
 }
 
-function operate(x, y, operator) {}
+function operate(x, y, operator) {
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return sub(x, y);
+    case "*":
+      return mul(x, y);
+    case "/":
+      return div(x, y);
+    default:
+      console.error(`[ERROR] Unrecognized operator: ${operator}`);
+      return;
+  }
+}
 
 let firstOperand;
 let secondOperand;
