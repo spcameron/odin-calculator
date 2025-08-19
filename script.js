4860f516bfb840f0040e1b6ebb714aaf96bbce04
spcameron <cameron.seanpatrick@gmail.com>
2025-08-19 19:04:44 -0400

    eslint and script.js WIP

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const firstOperand = "";
+const secondOperand = "";
+const currentOperator = null;
+
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operators");
+const upperDisplay = document.getElementById("upper-display");
+const lowerDisplay = document.getElementById("lower-display");
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => handleNumber(button.value)),
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => handleOperator(button.value)),
+);
+
+function clearLowerDisplay() {
+  lowerDisplay.textContent = "";
+}
+
+function clearUpperDisplay() {
+  upperDisplay.textContent = "";
+}
+
+function handleNumber(number) {
+  if (lowerDisplay.currentText === "0") clearLowerDisplay();
+
+  updateLowerDisplay(firstOperand, secondOperand, currentOperator);
+}
+
+function handleOperator() {}
+
+function updateLowerDisplay(firstNumber, secondNumber, operator) {
+  let displayText = `${firstNumber}`;
+
+  if (operator !== null) displayText += ` ${operator}`;
+
+  if (secondNumber !== "") displayText += ` ${secondNumber}`;
+
+  lowerDisplay.currentText = displayText;
+}
+
 function add(x, y) {
   return x + y;
 }
@@ -15,6 +58,9 @@ function div(x, y) {
 }
 
 function operate(x, y, operator) {
+  x = +x;
+  y = +y;
+
   switch (operator) {
     case "+":
       return add(x, y);
@@ -23,13 +69,10 @@ function operate(x, y, operator) {
     case "*":
       return mul(x, y);
     case "/":
+      if (y === 0) return null;
       return div(x, y);
     default:
       console.error(`ERROR: Unrecognized operator: ${operator}`);
-      return;
+      return null;
   }
 }
-
-let firstOperand;
-let secondOperand;
-let operator;
